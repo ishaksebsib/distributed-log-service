435ca0b5b0e0520ba0ddeb9b943d259d972a7cc5
ishaksebsib <ishaksebsib@gmail.com>
2025-02-25 20:12:33 +0300

    feat: add appState and router with gorilla/mux with log request and reponse handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ishaksebsib/distributed-log-service
 
 go 1.23.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ internal/server/http.go
@@ -0,0 +1,92 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type app struct {
+	Log *Log
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
+
+func newApp() *app {
+	return &app{
+		Log: NewLog(),
+	}
+
+}
+
+func (s *app) handleProduce(w http.ResponseWriter, r *http.Request) {
+	var req ProduceRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	off, err := s.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := ProduceResponse{Offset: off}
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *app) handleConsume(w http.ResponseWriter, r *http.Request) {
+	var req ConsumeRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	record, err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := ConsumeResponse{Record: record}
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func NewHTTPServer(addr string) *http.Server {
+	app := newApp()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", app.handleProduce).Methods("POST")
+	r.HandleFunc("/", app.handleConsume).Methods("GET")
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
