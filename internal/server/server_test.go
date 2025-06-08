0c8e8d93930a8c032ed0154ad1d72921898a64a4
ishaksebsib <ishaksebsib@gmail.com>
2025-06-08 20:48:55 +0300

    test: add test for grpc Log Stream

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -21,8 +21,9 @@ func TestServer(t *testing.T) {
 		client api.LogClient,
 		config *Config,
 	){
-		"produce/consume a message to/from the log using grpc succeeeds": testProduceConsume,
-		"consume a message that is out of range returns an error":        testConsumePastBoundary,
+		"produce/consume a message to/from the log using grpc succeeeds":            testProduceConsume,
+		"consume a message that is out of range returns an error":                   testConsumePastBoundary,
+		"produce/consume a stream of messages to/from the log using grpc succeeeds": testProduceConsumeStream,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			client, config, teardown := setupTest(t, nil)
@@ -128,5 +129,60 @@ func testConsumePastBoundary(
 	want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
 	if got != want {
 		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
+
+func testProduceConsumeStream(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+
+	records := []*api.Record{{
+		Value:  []byte("first message"),
+		Offset: 0,
+	}, {
+		Value:  []byte("second message"),
+		Offset: 1,
+	}}
+
+	{
+		stream, err := client.ProduceStream(ctx)
+		require.NoError(t, err)
+
+		for offset, record := range records {
+			err = stream.Send(&api.ProduceRequest{
+				Record: record,
+			})
+			require.NoError(t, err)
+
+			res, err := stream.Recv()
+			require.NoError(t, err)
+
+			if res.Offset != uint64(offset) {
+				t.Fatalf(
+					"got offset: %d, want: %d",
+					res.Offset,
+					offset,
+				)
+			}
+		}
+	}
+	{
+		stream, err := client.ConsumeStream(
+			ctx,
+			&api.ConsumeRequest{Offset: 0},
+		)
+		require.NoError(t, err)
+
+		for i, record := range records {
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			require.Equal(t, res.Record, &api.Record{
+				Value:  record.Value,
+				Offset: uint64(i),
+			})
+		}
 	}
 }
