595a013c73685fe394945159568560c5588219b6
ishaksebsib <ishaksebsib@gmail.com>
2025-06-08 20:39:10 +0300

    test: add test to consume and produce log using gRpc

---
+++ internal/server/server_test.go
@@ -0,0 +1,100 @@
+package server
+
+import (
+	"context"
+	"net"
+	"os"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	api "github.com/ishaksebsib/distributed-log-service/api/v1"
+	"github.com/ishaksebsib/distributed-log-service/internal/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log using grpc succeeeds": testProduceConsume,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(*Config)) (
+	client api.LogClient,
+	cfg *Config,
+	teardown func(),
+) {
+	t.Helper()
+	listener, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	clientOptions := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	cc, err := grpc.NewClient(listener.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+
+	dir, err := os.MkdirTemp("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	cfg = &Config{
+		CommitLog: clog,
+	}
+	if fn != nil {
+		fn(cfg)
+	}
+
+	server, err := NewGRPCServer(cfg)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	client = api.NewLogClient(cc)
+
+	return client, cfg, func() {
+		server.Stop()
+		cc.Close()
+		listener.Close()
+		clog.Remove()
+	}
+}
+
+func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
+	ctx := context.Background()
+
+	tempLog := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(
+		ctx,
+		&api.ProduceRequest{
+			Record: tempLog,
+		},
+	)
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset,
+	})
+	require.NoError(t, err)
+	require.Equal(t, tempLog.Value, consume.Record.Value)
+	require.Equal(t, tempLog.Offset, consume.Record.Offset)
+}
